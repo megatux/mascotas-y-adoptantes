1b7dc129ed9b88d3de2a6592dd1b8718c3439cbe
Cristian Molina <megatux@gmail.com>
2014-09-27 21:37:25 -0300

    Better Adopter test naming

--- test/models/adopter_test.rb
+++ test/models/adopter_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class AdopterTest < ActiveSupport::TestCase
   context "an adopter" do
     [:name, :description, :score].each do |attr|
-      should "be invalid missing with attribute #{attr}" do
+      should "be invalid without attribute #{attr}" do
         adopter = Adopter.new(attr.to_sym => nil)
         assert_not adopter.save
         assert adopter.errors[attr.to_sym].include?("can't be blank")
